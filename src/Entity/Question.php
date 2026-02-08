559373dab1ec352dba0800310e1f88d3153aacbd
fares8lefi <fareslefi4@gmail.com>
2026-02-08 20:17:41 +0100

    create question entity

---
+++ src/Entity/Question.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'id_question')]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'questions')]
+    #[ORM\JoinColumn(name: 'id_quiz', referencedColumnName: 'id_quiz', nullable: false)]
+    private ?Quiz $quiz = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $enonce = null;
+
+    #[ORM\Column(name: 'choix_a', length: 255)]
+    private ?string $choixA = null;
+
+    #[ORM\Column(name: 'choix_b', length: 255)]
+    private ?string $choixB = null;
+
+    #[ORM\Column(name: 'choix_c', length: 255)]
+    private ?string $choixC = null;
+
+    #[ORM\Column(name: 'choix_d', length: 255)]
+    private ?string $choixD = null;
+
+    #[ORM\Column(name: 'bonne_reponse', length: 1)]
+    private ?string $bonneReponse = null;
+
+    #[ORM\Column]
+    private ?int $points = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuiz(): ?Quiz
+    {
+        return $this->quiz;
+    }
+
+    public function setQuiz(?Quiz $quiz): static
+    {
+        $this->quiz = $quiz;
+
+        return $this;
+    }
+
+    public function getEnonce(): ?string
+    {
+        return $this->enonce;
+    }
+
+    public function setEnonce(string $enonce): static
+    {
+        $this->enonce = $enonce;
+
+        return $this;
+    }
+
+    public function getChoixA(): ?string
+    {
+        return $this->choixA;
+    }
+
+    public function setChoixA(string $choixA): static
+    {
+        $this->choixA = $choixA;
+
+        return $this;
+    }
+
+    public function getChoixB(): ?string
+    {
+        return $this->choixB;
+    }
+
+    public function setChoixB(string $choixB): static
+    {
+        $this->choixB = $choixB;
+
+        return $this;
+    }
+
+    public function getChoixC(): ?string
+    {
+        return $this->choixC;
+    }
+
+    public function setChoixC(string $choixC): static
+    {
+        $this->choixC = $choixC;
+
+        return $this;
+    }
+
+    public function getChoixD(): ?string
+    {
+        return $this->choixD;
+    }
+
+    public function setChoixD(string $choixD): static
+    {
+        $this->choixD = $choixD;
+
+        return $this;
+    }
+
+    public function getBonneReponse(): ?string
+    {
+        return $this->bonneReponse;
+    }
+
+    public function setBonneReponse(string $bonneReponse): static
+    {
+        $this->bonneReponse = $bonneReponse;
+
+        return $this;
+    }
+
+    public function getPoints(): ?int
+    {
+        return $this->points;
+    }
+
+    public function setPoints(int $points): static
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+}
