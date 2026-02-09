4472533f38f59e9f617eb3b800c20abbc572e341
fares8lefi <fareslefi4@gmail.com>
2026-02-09 11:18:00 +0100

    Add password column to user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,9 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -27,10 +29,13 @@ class User
     #[ORM\Column(length: 255)]
     private ?string $role = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
     /**
-     * @var Collection<int, cours>
+     * @var Collection<int, Cours>
      */
-    #[ORM\ManyToMany(targetEntity: cours::class, inversedBy: 'users')]
+    #[ORM\ManyToMany(targetEntity: Cours::class, inversedBy: 'users')]
     private Collection $cours;
 
     public function __construct()
@@ -91,15 +96,27 @@ class User
         return $this;
     }
 
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
     /**
-     * @return Collection<int, cours>
+     * @return Collection<int, Cours>
      */
     public function getCours(): Collection
     {
         return $this->cours;
     }
 
-    public function addCour(cours $cour): static
+    public function addCour(Cours $cour): static
     {
         if (!$this->cours->contains($cour)) {
             $this->cours->add($cour);
@@ -108,10 +125,43 @@ class User
         return $this;
     }
 
-    public function removeCour(cours $cour): static
+    public function removeCour(Cours $cour): static
     {
         $this->cours->removeElement($cour);
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $role = $this->role;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+        
+        if ($role === 'admin') {
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
     }
 }
