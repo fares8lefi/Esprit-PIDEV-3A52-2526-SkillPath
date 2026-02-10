cb7ba0d53bdf5c6cae00b61e49a18b96bbc7cb83
yazidkhediri89-collab <yazidkhediri89@gmail.com>
2026-02-10 15:09:07 -0800

    improve list design

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -1,78 +1,142 @@
-<?php
-
-namespace App\Controller;
+<?php 
+ namespace App\Controller;
 
 use App\Entity\Cours;
+use App\Entity\Module;
+use App\Form\CoursType;
 use App\Repository\CoursRepository;
+use App\Repository\ModuleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
-#[Route('/admin/cours')]
+#[Route('/admin/cours', name: 'admin_cours_')]
 class CoursController extends AbstractController
 {
-    // =========================
-    // LISTE DES COURS
-    // =========================
-    #[Route('/', name: 'cours_list')]
-    public function list(CoursRepository $repo): Response
-    {
+    #[Route('/', name: 'list', methods: ['GET'])]
+    public function list(
+        Request $request,
+        CoursRepository $coursRepository,
+        moduleRepository $moduleRepository,
+        PaginatorInterface $paginator
+    ): Response {
+        // Récupérer les filtres
+        $search = $request->query->get('search', '');
+        $moduleFilter = $request->query->get('module', '');
+        $typeFilter = $request->query->get('type', '');
+
+        // Créer la requête de base
+        $queryBuilder = $coursRepository->createQueryBuilder('c')
+            ->leftJoin('c.module', 'm')
+            ->addSelect('m');
+
+        // Appliquer les filtres
+        if ($search) {
+            $queryBuilder
+                ->andWhere('c.titre LIKE :search OR c.contenu LIKE :search')
+                ->setParameter('search', '%' . $search . '%');
+        }
+
+        if ($moduleFilter) {
+            $queryBuilder
+                ->andWhere('c.module = :module')
+                ->setParameter('module', $moduleFilter);
+        }
+
+        if ($typeFilter) {
+            $queryBuilder
+                ->andWhere('c.type = :type')
+                ->setParameter('type', $typeFilter);
+        }
+
+        // Trier par ID décroissant
+        $queryBuilder->orderBy('c.id', 'DESC');
+
+        // Paginer les résultats
+        $cours = $paginator->paginate(
+            $queryBuilder->getQuery(),
+            $request->query->getInt('page', 1),
+            10 // Nombre d'éléments par page
+        );
+
+        // Récupérer tous les modules pour le filtre
+        $modules = $moduleRepository->findAll();
+
         return $this->render('BackOffice/cours/list.html.twig', [
-            'cours' => $repo->findAll()
+            'cours' => $cours,
+            'modules' => $modules,
         ]);
     }
 
-    // =========================
-    // AJOUTER COURS (test auto)
-    // =========================
-    #[Route('/new', name: 'cours_new')]
-    public function new(EntityManagerInterface $em): Response
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $cours = new Cours();
-        $cours->setTitre('Nouveau cours');
-        $cours->setContenu('Contenu du cours');
-        $cours->setType('PDF');
+        $form = $this->createForm(CoursType::class, $cours);
+        $form->handleRequest($request);
 
-        $em->persist($cours);
-        $em->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($cours);
+            $entityManager->flush();
 
-        return $this->redirectToRoute('cours_list');
-    }
+            $this->addFlash('success', 'Le contenu a été ajouté avec succès.');
 
-    // =========================
-    // SUPPRIMER
-    // =========================
-    #[Route('/delete/{id}', name: 'cours_delete')]
-    public function delete($id, CoursRepository $repo, EntityManagerInterface $em): Response
-    {
-        $cours = $repo->find($id);
-
-        if (!$cours) {
-            return new Response('Cours introuvable');
+            return $this->redirectToRoute('admin_cours_list');
         }
 
-        $em->remove($cours);
-        $em->flush();
-
-        return $this->redirectToRoute('cours_list');
+        return $this->render('BackOffice/cours/new.html.twig', [
+            'cours' => $cours,
+            'form' => $form,
+        ]);
     }
 
-    // =========================
-    // MODIFIER (test simple)
-    // =========================
-    #[Route('/edit/{id}', name: 'cours_edit')]
-    public function edit($id, CoursRepository $repo, EntityManagerInterface $em): Response
+    #[Route('/{id}', name: 'show', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function show(Cours $cours): Response
     {
-        $cours = $repo->find($id);
+        return $this->render('BackOffice/cours/show.html.twig', [
+            'cours' => $cours,
+        ]);
+    }
 
-        if (!$cours) {
-            return new Response('Cours introuvable');
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'], requirements: ['id' => '\d+'])]
+    public function edit(Request $request, Cours $cours, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CoursType::class, $cours);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cours->setUpdatedAt(new \DateTime());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le contenu a été modifié avec succès.');
+
+            return $this->redirectToRoute('admin_cours_list');
         }
 
-        $cours->setTitre('Titre modifié');
-        $em->flush();
+        return $this->render('BackOffice/cours/edit.html.twig', [
+            'cours' => $cours,
+            'form' => $form,
+        ]);
+    }
 
-        return $this->redirectToRoute('cours_list');
+    #[Route('/{id}', name: 'delete', methods: ['POST'], requirements: ['id' => '\d+'])]
+    public function delete(Request $request, Cours $cours, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $cours->getId(), $request->request->get('_token'))) {
+            try {
+                $entityManager->remove($cours);
+                $entityManager->flush();
+                $this->addFlash('success', 'Le contenu a été supprimé avec succès.');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'Une erreur est survenue lors de la suppression.');
+            }
+        } else {
+            $this->addFlash('error', 'Token CSRF invalide.');
+        }
+
+        return $this->redirectToRoute('admin_cours_list');
     }
 }
--- src/Repository/CoursRepository.php
+++ src/Repository/CoursRepository.php
@@ -16,28 +16,102 @@ class CoursRepository extends ServiceEntityRepository
         parent::__construct($registry, Cours::class);
     }
 
-    //    /**
-    //     * @return Cours[] Returns an array of Cours objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * Recherche de contenus avec filtres
+     * 
+     * @param string|null $search Texte de recherche
+     * @param int|null $moduleId ID du module
+     * @param string|null $type Type de contenu
+     * @return Cours[]
+     */
+    public function findByFilters(?string $search = null, ?int $moduleId = null, ?string $type = null): array
+    {
+        $qb = $this->createQueryBuilder('c')
+            ->leftJoin('c.module', 'm')
+            ->addSelect('m');
 
-    //    public function findOneBySomeField($value): ?Cours
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        if ($search) {
+            $qb->andWhere('c.titre LIKE :search OR c.contenu LIKE :search')
+               ->setParameter('search', '%' . $search . '%');
+        }
+
+        if ($moduleId) {
+            $qb->andWhere('c.module = :module')
+               ->setParameter('module', $moduleId);
+        }
+
+        if ($type) {
+            $qb->andWhere('c.type = :type')
+               ->setParameter('type', $type);
+        }
+
+        return $qb->orderBy('c.id', 'DESC')
+                  ->getQuery()
+                  ->getResult();
+    }
+
+    /**
+     * Récupère les contenus par module
+     * 
+     * @param Module $module
+     * @return Cours[]
+     */
+    public function findByModule($module): array
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.module = :module')
+            ->setParameter('module', $module)
+            ->orderBy('c.id', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Récupère les contenus par type
+     * 
+     * @param string $type
+     * @return Cours[]
+     */
+    public function findByType(string $type): array
+    {
+        return $this->createQueryBuilder('c')
+            ->leftJoin('c.module', 'm')
+            ->addSelect('m')
+            ->andWhere('c.type = :type')
+            ->setParameter('type', $type)
+            ->orderBy('c.id', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Compte le nombre de contenus par type
+     * 
+     * @return array
+     */
+    public function countByType(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->select('c.type, COUNT(c.id) as total')
+            ->groupBy('c.type')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Récupère les derniers contenus créés
+     * 
+     * @param int $limit
+     * @return Cours[]
+     */
+    public function findLatest(int $limit = 10): array
+    {
+        return $this->createQueryBuilder('c')
+            ->leftJoin('c.module', 'm')
+            ->addSelect('m')
+            ->orderBy('c.createdAt', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
 }
