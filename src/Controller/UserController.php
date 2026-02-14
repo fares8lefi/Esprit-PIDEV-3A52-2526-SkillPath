9d2246b888703839bf5c57da51f30511eef8f6ea
fares8lefi <fareslefi4@gmail.com>
2026-02-14 21:30:51 +0100

    Save local changes on main before merging to User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -21,13 +21,8 @@ class UserController extends AbstractController
     #[Route('/login', name: 'app_user_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
+    
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('FrontOffice/user/login.html.twig', [
