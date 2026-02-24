3a5644347a8423554c023cc07040a8c481d297d0
yazidkhediri89-collab <yazidkhediri89@gmail.com>
2026-02-24 13:15:38 -0800

    ajout du système de génération de certificat PDF

---
+++ src/Entity/Certificate.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Cours;
+use App\Entity\User;
+use App\Repository\CertificateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CertificateRepository::class)]
+class Certificate
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $certificateNumber = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Cours $course = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCertificateNumber(): ?string
+    {
+        return $this->certificateNumber;
+    }
+
+    public function setCertificateNumber(string $certificateNumber): static
+    {
+        $this->certificateNumber = $certificateNumber;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCourse(): ?Cours
+    {
+        return $this->course;
+    }
+
+    public function setCourse(?Cours $course): static
+    {
+        $this->course = $course;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
