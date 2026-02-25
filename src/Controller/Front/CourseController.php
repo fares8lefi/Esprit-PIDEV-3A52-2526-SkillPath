1e4ca40525140ca0b7886d15e02a55c5329a33f4
fares8lefi <fareslefi4@gmail.com>
2026-02-25 16:16:28 +0100

    Add verification system when user connects to enroll in course

--- src/Controller/Front/CourseController.php
+++ src/Controller/Front/CourseController.php
@@ -12,24 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/course', name: 'front_course_')]
 class CourseController extends AbstractController
 {
-    #[Route('/', name: 'index', methods: ['GET'])]
-    public function index(Request $request, CourseRepository $courseRepository): Response
-    {
-        $search = $request->query->get('search');
-        $level = $request->query->get('level');
-        $category = $request->query->get('category');
-
-        $courses = $courseRepository->findByFilters($search, $level, $category);
-        $categoriesCount = $courseRepository->countByCategories();
-
-        return $this->render('FrontOffice/course/index.html.twig', [
-            'courses' => $courses,
-            'categoriesCount' => $categoriesCount,
-            'currentSearch' => $search,
-            'currentLevel' => $level,
-            'currentCategory' => $category,
-        ]);
-    }
 
     #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Course $course, \App\Service\UserCourseViewService $viewService): Response
