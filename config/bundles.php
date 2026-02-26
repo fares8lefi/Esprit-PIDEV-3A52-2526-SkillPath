87ed40a39d279fdfd0c683abd5ccae68afd9caf9
yazidkhediri89-collab <yazidkhediri89@gmail.com>
2026-02-26 13:28:30 -0800

    Config project environment and dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
+    CalendarBundle\CalendarBundle::class => ['all' => true],
 ];
