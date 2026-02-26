182ec61a448da9749ab556eb9eb4292d4ea172a6
yazidkhediri89-collab <yazidkhediri89@gmail.com>
2026-02-26 08:36:33 -0800

    nouvelle_branche_cours

---
+++ src/Controller/Front/NotificationController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\NotificationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/notifications', name: 'app_notifications_')]
+class NotificationController extends AbstractController
+{
+    #[Route('/unread-count', name: 'unread_count', methods: ['GET'])]
+    public function getUnreadCount(NotificationRepository $repository): JsonResponse
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return new JsonResponse(['count' => 0]);
+        }
+
+        return new JsonResponse(['count' => count($repository->findUnreadByUser($user))]);
+    }
+
+    #[Route('/list', name: 'list', methods: ['GET'])]
+    public function list(NotificationRepository $repository): JsonResponse
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return new JsonResponse(['notifications' => []]);
+        }
+
+        $notifications = $repository->findBy(['user' => $user], ['createdAt' => 'DESC'], 5);
+        $data = [];
+        foreach ($notifications as $n) {
+            $data[] = [
+                'id' => $n->getId(),
+                'title' => $n->getTitle(),
+                'message' => $n->getMessage(),
+                'createdAt' => $n->getCreatedAt()->format('d/m H:i'),
+                'isRead' => $n->isRead(),
+                'link' => $n->getLink()
+            ];
+        }
+
+        return new JsonResponse(['notifications' => $data]);
+    }
+
+    #[Route('/mark-as-read/{id}', name: 'mark_as_read', methods: ['POST'])]
+    public function markAsRead(int $id, NotificationRepository $repository, EntityManagerInterface $em): JsonResponse
+    {
+        $notification = $repository->find($id);
+        if (!$notification || $notification->getUser() !== $this->getUser()) {
+            return new JsonResponse(['success' => false], 404);
+        }
+
+        $notification->setIsRead(true);
+        $em->flush();
+
+        return new JsonResponse(['success' => true]);
+    }
+}
---
+++ src/EventListener/ModuleAddedListener.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Module;
+use App\Service\NotificationService;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Psr\Log\LoggerInterface;
+
+#[AsDoctrineListener(event: Events::postPersist)]
+class ModuleAddedListener
+{
+    private $notificationService;
+    private $logger;
+
+    public function __construct(NotificationService $notificationService, LoggerInterface $logger)
+    {
+        $this->notificationService = $notificationService;
+        $this->logger = $logger;
+    }
+
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        $module = $args->getObject();
+        if (!$module instanceof Module) {
+            return;
+        }
+
+        $this->logger->info("ModuleAddedListener: Triggered for module " . $module->getId());
+
+        $course = $module->getCourse();
+        if (!$course) {
+            $this->logger->warning("ModuleAddedListener: No course found for module " . $module->getId());
+            return;
+        }
+
+        $users = $course->getUsers();
+        $this->logger->info("ModuleAddedListener: Found " . count($users) . " users for course " . $course->getId());
+
+        if (count($users) === 0) {
+            return;
+        }
+
+        $title = "Nouveau module ajouté !";
+        $message = sprintf(
+            'Un nouveau module intitulé "%s" a été ajouté au cours "%s".',
+            $module->getTitle(),
+            $course->getTitle()
+        );
+        
+        $link = "/course/" . $course->getId(); 
+
+        try {
+            $this->notificationService->notifyUsers($users, $title, $message, $link);
+            $this->logger->info("ModuleAddedListener: Notifications sent successfully");
+        } catch (\Exception $e) {
+            $this->logger->error("ModuleAddedListener: Error sending notifications: " . $e->getMessage());
+        }
+    }
+}
